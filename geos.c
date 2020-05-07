31335cfb0c00c17d7672e79518406e1efedb9555
Fatih Aytekin <fatih@aytekin.me>
2020-05-08 02:35:11 +0300

    Fix variable type

--- geos.c
+++ geos.c
@@ -863,7 +863,7 @@ PHP_METHOD(Geometry, buffer)
     GEOS_PHP_ZVAL data;
     HashTable *style;
     zend_string *key;
-    ulong index;
+    zend_ulong index;
 
     this = (GEOSGeometry*)getRelay(getThis(), Geometry_ce_ptr);
 
@@ -956,7 +956,7 @@ PHP_METHOD(Geometry, offsetCurve)
     GEOS_PHP_ZVAL data;
     HashTable *style;
     zend_string *key;
-    ulong index;
+    zend_ulong index;
 
     this = (GEOSGeometry*)getRelay(getThis(), Geometry_ce_ptr);
 
