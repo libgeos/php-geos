09c4a3bded6bc299ce118e8f14512cc533b45f5c
masrih <H.Masri@dvz-mv.de>
2023-11-30 09:18:55 +0100

    removing unnecessary test file

--- geos_version_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Call GEOSVersion() function to get the GEOS version
-$geosVersionString = GEOSVersion();
-
-// Output the GEOS version for verification
-echo "GEOS Version: $geosVersionString\n";
